93102220250cd6a4254495c5a9e1d51ea36de3f0
Evren Yucekus-Kissane <83101246+EvrenKissane@users.noreply.github.com>
2021-09-26 19:37:25 -0400

    Delete ultimate-saas-ts directory

--- ultimate-saas-ts/types/index.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest } from 'next';
-import { Session } from 'next-auth';
-
-export interface AppNextApiRequest extends NextApiRequest {
-  session?: Session & {
-    userId?: string;
-  };
-}
--- ultimate-saas-ts/utils/ssr/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as prisma } from './prisma';
-export { default as stripe } from './stripe';
-export * from './createApiHandler';
